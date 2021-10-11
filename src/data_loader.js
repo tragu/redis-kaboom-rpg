244934fe7589a5d8a72810d3fe96065f21713429
Rahul Gupta Tankasala <tankasalarahulgupta@gmail.com>
2021-10-11 19:10:23 +0530

    Update data_loader.js

--- src/data_loader.js
+++ src/data_loader.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis');
 
-const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_HOST = process.env.REDIS_HOST || 'host.docker.internal';
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
